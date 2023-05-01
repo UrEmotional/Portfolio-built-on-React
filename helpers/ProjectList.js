c1d78ae443e612a281350eaafc8fdfd32187d4ae
Jun <118232823+UrEmotional@users.noreply.github.com>
2023-04-30 21:40:38 -0500

    The good stuff

---
+++ helpers/ProjectList.js
@@ -0,0 +1,29 @@
+import proj1 from "../assets/images/proj1.png";
+import proj2 from "../assets/images/proj2.png";
+import proj3 from "../assets/images/proj3.png";
+import proj4 from "../assets/images/proj4.png";
+
+export const projectList = [
+{
+    name: "Anime Api Site",
+    image: proj1,
+    skills: "Html, CSS, JavaScript, Bootstrap, Jikan API, Bulma"
+},
+{
+    name: "Note Taker",
+    image: proj2,
+    skills: "Html, CSS, JavaScript, express, node, uuid"
+},
+{
+    name: "Weather Dashboard",
+    image: proj3,
+    skills: "Html, CSS, JavaScript, Bootstrap, OpenWeather API"
+},
+{
+    name: "Back-End Employee Database",
+    image: proj4,
+    skills: " JavaScript, Node, Inquirer, MySQL, express"
+},
+]
+
+export default projectList;
